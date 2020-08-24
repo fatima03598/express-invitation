5ef9f92e752aa25e58583e16f5e24c25964cfffc
fatima03598 <54324041+fatima03598@users.noreply.github.com>
2020-08-24 16:02:04 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# express-invitation-2
+# express-invitation
+
+## Description
+Simple invitation card generator with node.js 
+
+![invitation](./invitation.gif)
+
+## Main Technologies & Language
+* JavaScript
+* Node.js
+* Express.js
+* Ejs
--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@ const express = require("express"); //assigns express
 const app = express(); //executes express
 const PORT = process.env.PORT || 4001;
 
+
 app.use(express.urlencoded({extended: false})); //Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
-app.use(express.static( 'public'));//Create a new middleware function to serve files from within a given root directory.
+;//Create a new middleware function to serve files from within a given root directory.
 //app.use("/", express.static(__dirname + '/html'))
+
 app.use(express.static("views"));
 app.set('view engine', 'ejs');  
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static( 'public'));
 
 app.post("/invitation",(req,res,next) => {
     const greeting = req.body.greeting;
